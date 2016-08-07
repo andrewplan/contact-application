6f5bd6e9ddffdf4b4f4e5760cbc788d76b69e523
andrewplan <a.plan88@gmail.com>
2016-08-07 17:31:14 -0500

    Fixed object storing, still need to fix contact display.

--- js/app.js
+++ js/app.js
@@ -1,193 +1,78 @@
 var allContacts = [];
 
-function getFirstName() {
-    var firstName = document.getElementById("first-name").value;
-    return firstName;
-}
-
-function getLastName() {
-    var lastName = document.getElementById("last-name").value;
-    return lastName; 
-}
-
-function getHomePhone() {
-    var homePhone = document.getElementById("home-phone").value;
-    return homePhone;
-}
-
-function getWorkPhone() {
-    var workPhone = document.getElementById("work-phone").value;
-    return workPhone;
-}
-
-function getMobilePhone() {
-    var mobilePhone = document.getElementById("mobile-phone").value;
-    return mobilePhone;
-}
-
-function getOtherPhoneNumbers() {
-    var allOtherPhoneNumbers = [];
-    for (var i = 0; i < allOtherPhoneNumbers.length; i++) {
-        allOtherPhoneNumbers[i] = document.getElementsByClassName("other-phone-number")[i].value;
-    }
-    return allOtherPhoneNumbers;
-}
-
-function getHomeEmail() {
-    var homeEmail = document.getElementById("home-email").value;
-    return homeEmail;
-}
-
-function getWorkEmail() {
-    var workEmail = document.getElementById("work-email").value;
-    return workEmail;
-}
-
-function getOtherEmailAddresses() {
-    var allOtherEmailAddresses = [];
-    for (var i = 0; i < allOtherEmailAddresses.length; i++) {
-        allOtherEmailAddresses[i] = document.getElementsByClassName("other-email-address")[i].value;
-    }
-    return allOtherEmailAddresses;
-}
-
-function getAddressType() {
-    var addressType = [];
-    for (var i = 0; i < addressType.length; i++) {
-        addressType[i] = document.getElementsByClassName("address-type")[i].value;
-    }
-    return addressType;
-}
-
-function getStreetLine1() {
-    var streetLine1 = [];
-    for (var i = 0; i < streetLine1.length; i++) {
-        streetLine1[i] = document.getElementsByClassName("street-line1")[i].value;
-    }
-    return streetLine1;
-}
-
-function getStreetLine2() {
-    var streetLine2 = [];
-    for (var i = 0; i < streetLine2.length; i++) {
-        streetLine2[i] = document.getElementsByClassName("street-line2")[i].value;
-    }
-    return streetLine2;
-}
-
-function getState() {
-    var state = [];
-    for (var i = 0; i < state.length; i++) {
-        state[i] = document.getElementsByClassName("state")[i].value;
-    }
-    return state;
-}
-
-function getCity() {
-    var city = [];
-    for (var i = 0; i < city.length; i++) {
-        city[i] = document.getElementsByClassName("city")[i].value;
-    }
-    return city;
-}
-
-function getCountry() {
-    var country = [];
-    for (var i = 0; i < country.length; i++) {
-        country[i] = document.getElementsByClassName("country")[i].value;
-    }
-    return country;
-}
-
-//code that adds as many "other" phone fields as the user desires, with additional accompanying contact.contact object to contact (creates contact.contact array)
-function addOtherPhoneNumberField() {
-    event.preventDefault();
-    $("#phone-numbers-container").append("<div class=\"other-phone-number-form\"><label for=\"other-phone-number\">Other phone number</label><br /><input type=\"tel\" class=\"other-phone-number\" class=\"all-inputs\" name=\"other-phone-number\" /></div>");
-    console.log("It works!");
-}
-
-function addOtherEmailAddressField() {
-    event.preventDefault();
-    $("#email-addresses-container").append("<div class=\"other-email-address-form\"><label for=\"other-email-address-form\">Other email address</label><br /><input type=\"text\" class=\"other-email-address\" class=\"all-inputs\" name=\"other-email\"></div>");
-    console.log("it works!")
-}
-
-//function that adds as many "other" address fields as the user desires, with additional accompanying address object to contact (creates address array)
-function addAddressField() {
-    event.preventDefault();
-    $("#addresses-container").append("<div class=\"address-form\"><div><label for=\"address-type\">Address type</label><br /><input type=\"text\" class=\"address-type all-inputs\" name=\"address-type\" /></div><div><label for=\"street-line1\">Street line 1</label><br /><input type=\"text\" class=\"street-line1 all-inputs\" name=\"street-line1\" /></div><div><label for=\"street-line2\">Street line 2</label><br /><input type=\"text\" class=\"street-line2 all-inputs\" name=\"street-line2\" /></div><div><label for=\"city\">City</label><br /><input type=\"text\" class=\"city all-inputs\" name=\"city\" /></div><div><label for=\"state\">State</label><br /><input type=\"text\" class=\"state all-inputs\" name=\"state\" /></div><div><label for=\"country\">Country</label><br /><input type=\"text\" class=\"country all-inputs\" name=\"country\" /></div></div>")
-}
-    
 var Contact = {
-    "Person": 
-    {
-        "firstName": "", 
-        "lastName": ""
-    },
-    "ContactMethod":
-    {
-        "homePhone": "",
-        "workPhone": "",
-        "mobilePhone": "",
-        "otherPhoneNumbers": [], 
-        "homeEmail": "",
-        "workEmail": "",
-        "otherEmailAddresses": []
-    }, 
-    "Addresses": [] 
+    "Names": [] ,
+    // names[0] = first name, names[1] = last name
+    "Phones": { "personal": [], "work": [] }, 
+    "Emails": { "personal": [], "work": [] }, 
+    "Addresses": {
+        "home": [{
+            "streetLine1": "",
+            "streetLine2": "",
+            "city": "",
+            "state": "",
+            "zip": "",
+            "country": ""
+        }],
+        "work": [{
+            "streetLine1": "",
+            "streetLine2": "",
+            "city": "",
+            "state": "",
+            "zip": "",
+            "country": ""
+        }]
+    }
 };
 
-var Address = {
-    "addressType": "",
-    "streetLine1": "",
-    "streetLine2": "",
-    "city": "",
-    "state": "",
-    "country": "",
-}; 
+function getValuesFromInputClass(theClass) {
+    var theItems = $("." + theClass).tagsinput('items');
+    console.log(theItems);
+    return theItems;
+}
+
+function addNamesToContact(currentContact) {
+    currentContact.Names = getValuesFromInputClass("name");
+}
     
-function addContactMethodToContact(currentContact) {
-    currentContact.ContactMethod.homePhone = getHomePhone();
-    currentContact.ContactMethod.workPhone = getWorkPhone();
-    currentContact.ContactMethod.mobilePhone = getMobilePhone();
-    currentContact.ContactMethod.homeEmail = getHomeEmail();
-    currentContact.ContactMethod.workEmail = getWorkEmail();
-    
-    var otherPhoneNumbers = getOtherPhoneNumbers();
-    var otherEmailAddresses = getOtherEmailAddresses();
-    
-    for (var i = 0; i < otherPhoneNumbers.length; i++) { 
-        currentContact.ContactMethod.otherPhoneNumbers.push(otherPhoneNumbers[i]);  
-    } 
-    
-    for (var i = 0; i < otherEmailAddresses.length; i++) {              
-        currentContact.ContactMethod.otherEmailAddresses.push(otherEmailAddresses[i]);
-    }
+function addContactMethodsToContact(currentContact) {
+    currentContact.Phones.personal = getValuesFromInputClass("personal-phones");
+    currentContact.Phones.work = getValuesFromInputClass("work-phones");
+    currentContact.Emails.personal = getValuesFromInputClass("personal-emails");
+    currentContact.Emails.work = getValuesFromInputClass("work-emails");
 }
 
 function addAddressesToContact(currentContact) {
-    var addressTypeArray = getAddressType();
-    var streetLine1Array = getStreetLine1();
-    var streetLine2Array = getStreetLine2();
-    var cityArray = getCity();
-    var stateArray = getState();
-    var countryArray = getCountry();
+    var homeAddress = getValuesFromInputClass("home-address");
+    currentContact.Addresses.home.streetLine1 = homeAddress[0];
+    currentContact.Addresses.home.streetLine2 = homeAddress[1];
+    currentContact.Addresses.home.city = homeAddress[2];
+    currentContact.Addresses.home.state = homeAddress[3];
+    currentContact.Addresses.home.zip = homeAddress[4];
+    currentContact.Addresses.home.country = homeAddress[5];
     
-    var listOfAddresses = [];
+    var workAddress = getValuesFromInputClass("work-address");
+    currentContact.Addresses.work.streetLine1 = workAddress[0];
+    currentContact.Addresses.work.streetLine2 = workAddress[1];
+    currentContact.Addresses.work.city = workAddress[2];
+    currentContact.Addresses.work.state = workAddress[3];
+    currentContact.Addresses.work.zip = workAddress[4];
+    currentContact.Addresses.work.country = workAddress[5];
     
-    for (var i = 0; i < listOfAddresses.length; i++) {
-        var newAddress = Object.create("Address");
-        
-        newAddress.addressType = addressTypeArray[i];
-        newAddress.streetLine1 = streetLine1Array[i];
-        newAddress.streetLine2 = streetLine2Array[i];
-        newAddress.country = countryArray[i];
-        newAddress.state = stateArray[i];
-        newAddress.city = cityArray[i];
-        
-        listOfAddresses.push(newAddress);
-        currentContact.Addresses.push(newAddress);
-    };
+    //code for loop that adds other addresses to Addresses object
+    /* var arrayOfOtherAddressses = getValuesFromInputClass("other-address");
+    console.log(arrayOfOtherAddressses);
+    for (var i = 0; i < arrayOfOtherAddressses.length; i++) {
+        currentContact.Addresses.other.streetLine1 = arrayOfOtherAddress[i][0];
+        currentContact.Addresses.other.streetLine2 = arrayOfOtherAddress[i][1];
+        currentContact.Addresses.other.city = arrayOfOtherAddress[i][2];
+        currentContact.Addresses.other.state = arrayOfOtherAddress[i][3];
+        currentContact.Addresses.other.zip = arrayOfOtherAddress[i][4];
+        currentContact.Addresses.other.country = arrayOfOtherAddress[i][5];
+    } 
+    return itemValues;
+    console.log(itemValues); */
+    
 }
 
 //function that pushes data entered into new contact object that is pushed to contacts array
@@ -195,31 +80,27 @@ function storeContactInfo() {
     event.preventDefault();
     var newContact = Object.create(Contact);
     
-    //grab values from form and store in new object "newContact.person"
-    newContact.Person.firstName = getFirstName();
-    newContact.Person.lastName = getLastName();
-    
-    //grab values from form and store in new object "newContact.contact"
-    addContactMethodToContact(newContact);
-    
-    //grab values from form and store in new object "newContact.address"
+    addNamesToContact(newContact);
+    addContactMethodsToContact(newContact);
     addAddressesToContact(newContact);
     
-    //push newContact object to allContacts array
     allContacts.push(newContact);
     console.log(allContacts);
+    
+    $("#contacts-list").show();
+    $("#contacts-list").append("<li>" + newContact.Names[0] + " " + newContact.Names[1] + "</li>");
 }
 
-// code for adding additional properties of vCard (begin, version, rev, end) to contact object
-        
-/* $("#contacts-list").show();
-$("#contacts-list").append("<li id='" + i + "'>" + personFirstName + " " + personLastName + "</li>");
-
-i++; */
+// code for adding additional properties of vCard (begin, version, rev, end) to contact object 
     
 //clears fields for additional contact input(s)
     $(".all-inputs").val("");
 
+//function that adds as many "other" address fields as the user desires
+function addAddressField() {
+    event.preventDefault();
+    $("#addresses-container").append("<div><label for=\"other-address\"> Enter other address in the following format: <ul><li>street line 1, street line 2, city, state, zip, country</li><li>type \"none\" for any fields that don't apply</li></ul></label><br /><input type=\"text\" class=\"other-address all-inputs bootstrap-tagsinput tag\" name=\"other-address\" data-role=\"tagsinput\"/></div>")
+}
 
 function hideContactDisplay() {
     event.preventDefault();
@@ -228,27 +109,15 @@ function hideContactDisplay() {
 
 //identify additional addresses and phone numbers by index number in array of addresses, array of phone numbers
 function fillInContactDisplay(currentContact) {
-    $("#full-name-display").text(currentContact.first_name + " " + currentContact.last_name);
-    $("#first-name-display").text(currentContact.first_name);
-    $("#last-name-display").text(currentContact.last_name);
+    $("#full-name-display").text(currentContact.Names[0] + " " + newContact.Names[1]);
     
-    $("#phone-number-1-display").text(currentContact.phone_numbers[0].phone_number);
-    
-    if (currentContact.phone_numbers[1].phone_number !== "") {
-        $("#phone-number-2-display").text(currentContact.phone_numbers[1].phone_number).show();
-    }
-    
-    $("#address-1-display").text(currentContact.addresses[0].street + ", " + currentContact.addresses[0].city + ", " + currentContact.addresses[0].state)
-    
-    if (currentContact.addresses[1].street !== "" ||  currentContact.addresses[1].city !== "" || currentContact.addresses[1].state !== "") {
-        $("#address-2-display").text(currentContact.addresses[1].street + ", " + currentContact.addresses[1].city + ", " + currentContact.addresses[1].state).show();
-    }   
+    ("#first-name-display").text(currentContact.Names[0]);
+    $("#last-name-display").text(currentContact.Names[1]);
 }
 
 $("#contacts-list").on('click', 'li', function(event){
     $("#contact-display").show();
     //function that fills in the values of the contact display with the array element corresponding to the li
-    var selectedID = event.target.id;
-    var selectedContact = contacts[selectedID];
+    var selectedContact = allContacts.this;
     fillInContactDisplay(selectedContact);
 });
